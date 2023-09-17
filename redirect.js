c1ea17ae8c0b281ff039b225d4790b9b777e6d91
Aeolus <aeolusdeveloper@gmail.com>
2023-09-17 20:00:47 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -16,5 +16,6 @@ submitButton.addEventListener('click', function() {
             action: "checkpass",
             msg: "CorrectPass"
         })
+        console.log("Operation Complete!")
     }
 });
