c4da9a745ab176c98e3b39baaec001a65c81fd01
Aeolus <aeolusdeveloper@gmail.com>
2023-09-18 20:28:44 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,7 +2,7 @@
 const userInput = document.getElementById('userInput');
 const submitButton = document.getElementById('submitButton');
 const output = document.getElementById('output');
-const pass = "NzU0OA=="; //PS: I don't use this anywhere else so don't think you got my passcode.
+const pass = "Mzc2NQ=="; //PS: I don't use this anywhere else so don't think you got my passcode.
 let decodedpass = atob(pass);
 // Add an event listener to the submit button
 submitButton.addEventListener('click', function() {
